eab128d4a3c10da050fb9d672828eec695e5dda5
“JulianBoralli” <jboralli@icloud.com>
2016-10-11 10:17:38 -0700

    Add code to SodaMachine class to make test pass

--- soda_machine.rb
+++ soda_machine.rb
@@ -2,17 +2,23 @@ class SodaMachine
   attr_reader :sodas, :cash
 
   def initialize(args = {})
-    @sodas = args[:sodas]
-    @cash = args[:cash]
+    @sodas = args[:sodas] # array of sodas
+    @cash = args[:cash] # Float
   end
 
   def current_inventory_count
+    @sodas.size
   end
 
   def find_soda(soda_brand)
+    @sodas.find {|soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    index = @sodas.find_index {|soda| soda.brand == soda_brand}
+    return nil if !index
+    @cash += @sodas[index].price
+    @sodas.delete_at(index)
   end
 
 end

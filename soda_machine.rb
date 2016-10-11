aeca8e4fdd009f0c0ff230f5986ecc510baa119f
Zara Aslam <zaslam72@gmail.com>
2016-10-11 10:35:25 -0700

    checkpoint challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,4 +1,6 @@
-class SodaMachine
+require_relative 'soda'
+
+class SodaMachine < Soda
   attr_reader :sodas, :cash
 
   def initialize(args = {})
@@ -7,12 +9,23 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.length
   end
 
   def find_soda(soda_brand)
+    sodas.find{|brand| brand = soda_brand}
+
+    
+
+# soda is initiated in Soda class 
+# takes in brand and price 
+# we need to iterate through the brands to find the soda brand
+
+
   end
 
   def sell(soda_brand)
+
   end
 
 end

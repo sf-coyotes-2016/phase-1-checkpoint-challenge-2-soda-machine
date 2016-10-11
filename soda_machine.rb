ede94b95bf496ad3efc080ab83cf5fc03716ab1e
Dana Cho <im.dana@gmail.com>
2016-10-11 10:43:55 -0700

    Checkpoint challenge 2

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,5 +1,7 @@
-class SodaMachine
-  attr_reader :sodas, :cash
+require 'soda'
+
+class SodaMachine < Soda
+  attr_accessor :sodas, :cash
 
   def initialize(args = {})
     @sodas = args[:sodas]
@@ -7,12 +9,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    # soda_machine = ["pepsi", "mountain_dew", "coke_zero", "second_pepsi"]
+    # soda_machine.count 
+    @sodas.keys.count
   end
 
   def find_soda(soda_brand)
+
+    #.find on the sodas array to find specific brand
+    #.pop to remove first 
+    @sodas.find(soda_brand).pop
+    @sodas.keys.pop
+
   end
 
   def sell(soda_brand)
+    return nil if @sodas.find(soda_brand) == 0 
+    else
+
+     @sodas.find(soda_brand) 
+
   end
 
 end

2dc7ce79cbcc9361f768a9e2eec88f8de475d506
Timothy Tao <timctao@gmail.com>
2016-10-11 10:35:10 -0700

    Define count_inventory method

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,5 +1,7 @@
+require_relative 'soda'
+
 class SodaMachine
-  attr_reader :sodas, :cash
+  attr_reader :sodas, :cash, :inventory_count
 
   def initialize(args = {})
     @sodas = args[:sodas]
@@ -7,12 +9,19 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas[0]
   end
 
   def sell(soda_brand)
   end
 
 end
+
+# pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
+# p pepsi.brand
+# soda_machine = SodaMachine.new(sodas: [pepsi], cash: 1.00)
+# p soda_machine.find_soda('Pepsi')

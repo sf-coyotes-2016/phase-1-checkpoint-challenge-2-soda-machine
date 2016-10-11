0f580e91b8ea47fed64f24f54f2708aac4d268f5
Nathan Herrera <nathanherrera@gmail.com>
2016-10-11 10:36:48 -0700

    Implement behavior to pass tests
    -Pass tests up to sell method...can return soda, but having trouble updating cash instance variable

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,16 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.length
   end
 
   def find_soda(soda_brand)
+    sodas.find { |soda| soda.brand == soda_brand }
   end
 
   def sell(soda_brand)
+    soda = find_soda(soda_brand)
+    # cash += soda.price
   end
 
 end

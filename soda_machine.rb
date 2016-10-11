a2934e3b64bf0bee8053cc654f0a754643ca06cb
yulkalongneck <juliadolgoszyja@gmail.com>
2016-10-11 10:35:30 -0700

    Add methods of soda machine

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,4 +1,4 @@
-class SodaMachine
+class SodaMachine < Soda
   attr_reader :sodas, :cash
 
   def initialize(args = {})
@@ -7,12 +7,25 @@ class SodaMachine
   end
 
   def current_inventory_count
+   @sodas.size
+
   end
 
   def find_soda(soda_brand)
+    if soda = @sodas.find {|soda| soda.brand == soda_brand}
+      return soda
+    else nil
+    end
   end
 
   def sell(soda_brand)
+    # if @sodas "does not contain this" soda_brand
+    # return nil
+    # elsif
+    return nil if @sodas.find {|soda| soda.brand != soda_brand}
+  
+
+
   end
 
 end

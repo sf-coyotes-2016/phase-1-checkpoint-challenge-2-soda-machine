e8284a0a5ccac17117565d29bf72d54daf1caf14
Katie O'Neill <katiemarieoneill@gmail.com>
2016-10-11 10:34:36 -0700

    Create method to return how many sodas there are; make method to return first soda.

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,20 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.length
   end
 
   def find_soda(soda_brand)
+    sodas.first
+    # sodas.find {|i| i }
   end
 
   def sell(soda_brand)
+
   end
 
 end
+
+#A soda machine will be able to return the number of
+#sodas it contains. This method should be called
+#SodaMachine#current_inventory_count.

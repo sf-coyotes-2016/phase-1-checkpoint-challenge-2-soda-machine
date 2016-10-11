3812223b082a485708d5698d71a64db1f41c3e6b
DL804 <darren804@gmail.com>
2016-10-11 10:07:50 -0700

    add psuedocode

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,6 +7,7 @@ class SodaMachine
   end
 
   def current_inventory_count
+    #count of each element\object in the sodas array 
     sodas.count
   end
 
@@ -16,7 +17,9 @@ class SodaMachine
   end
 
   def sell(soda_brand)
-    #use method above which will return a found soda
+    # use method above which will return a found soda object
+    # if found, add object's price to the cash array
+    # delete the soda object from the sodas array
     found_soda = find_soda(soda_brand)
     if found_soda
       @cash += found_soda.price

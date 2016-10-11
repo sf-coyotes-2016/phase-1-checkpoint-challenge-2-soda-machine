1c680d386d10360139af3109e481d5b47608b229
DL804 <darren804@gmail.com>
2016-10-11 10:03:02 -0700

    final commit

--- soda_machine.rb
+++ soda_machine.rb
@@ -11,7 +11,8 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
-    @sodas.find { |soda| soda.brand == soda_brand}
+    #use enumerable find method which will return nil if no soda is found
+    sodas.find { |soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)

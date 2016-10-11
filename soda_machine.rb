f6f562b247e0daaf3cc2f1b5787057aadf99c823
Tony Nguyen <tntuyen@ucdavis.edu>
2016-10-11 10:35:28 -0700

    checkpoint challenge -Tony

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,31 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
-  def find_soda(soda_brand)
+   def find_soda(soda_brand)
+    sodas.each do |soda|
+      if soda.brand == soda_brand 
+        return soda 
+      else
+        puts "Sorry, that brand is unavailable."
+      end
+    end
   end
+  
+  def self.brand
+    return self
+  end
+
 
   def sell(soda_brand)
+    @sodas.each do |soda|
+      if soda.brand == soda_brand 
+        sodas.slice!(soda)
+      end
+    end
+
   end
 
 end

9a705edc8b24e764c3db9af99d0c3d3da8ea4e39
Zara Aslam <email": null>
2016-10-11 20:26:11 -0700

    made tests pass

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,21 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.length
   end
 
   def find_soda(soda_brand)
+    sodas.find {|soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    soda_bought = sodas.find { |soda| soda.brand == soda_brand }
+    if soda_bought != nil
+      @cash += soda_bought.price
+      sodas.delete(soda_bought)
+    else
+      return nil
+    end
   end
-
 end
+

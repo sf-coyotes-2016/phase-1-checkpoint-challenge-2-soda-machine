8d26d8ed32ba6d863c836f85b709e2323f24140e
JessCodes <jessicafellison@gmail.com>
2016-10-18 21:03:05 -0700

    Add sell method with conditional statement

--- soda_machine.rb
+++ soda_machine.rb
@@ -14,7 +14,12 @@ class SodaMachine
     @sodas.find { |soda| soda.brand == soda_brand }
   end
 
-  def sell(soda_brand)
+  def sell(soda_brand) 
+    if find_soda(soda_brand)
+      soda_to_sell = find_soda(soda_brand)
+      @cash += soda_to_sell.price
+      @sodas.delete(soda_to_sell)
+    end
   end
 
 end
